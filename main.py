6e1987d426923a7db9c5836f2540788cb887f64a
Sadaqat <sadkb.559@gmail.com>
2025-07-13 13:27:45 +0500

    init commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 class product:
-    def __init__(self, name, price, sku, quantity_in_stocs):
+    def __init__(self, name, price, sku, quantity_in_stocks):
         self.name = name
         self.price  = price
         self.sku = sku
-        self.quantity_in_stocks = quantity_in_stocs
+        self.quantity_in_stocks = quantity_in_stocks
         
     def display_info(self):
         print(f"product: {self.name}")
@@ -18,3 +18,39 @@ class product:
         else:
             print(f"Insufficient stock. only {self.quantity_in_stocks} items available. ")
                     
+                    
+class customer:
+    def __init__(self, customer_id, name, email):
+        self.customer_id = customer_id
+        self.name = name
+        self.email = email
+        self.cart = []
+        
+        
+    def view_profile(self):
+        print(f"customer_id: {self.customer_id}")
+        print(f"name: {self.name}")
+        print(f"email: {self.email}")
+        
+        
+    def add_to_cart(self, product, quantity):
+        self.cart.append((product, quantity))
+        print(f"added {quantity} of {product.name} to cart.")
+        
+        
+    def view_cart(self):
+        if not self.cart:
+            print("cart is empty. ")
+        else:
+            print("cart items: ")
+            for item, quantity in self.cart:
+                print(f"- {item.name}  (x{quantity}) - ${item.price} each")
+                
+    def clear_cart(self):
+        self.cart = []
+        print("cart has been cleared")   
+        
+        
+        
+
+
